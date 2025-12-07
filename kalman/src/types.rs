905cefdcefcc07e1ce3796c7847f2f20c6a71ddb
Parker Barrett <parkerb7@vt.edu>
2025-12-07 12:20:53 -0500

    Initial commit

---
+++ kalman/src/types.rs
@@ -0,0 +1,35 @@
+//! Kalman Filter Types
+use nalgebra::{SMatrix, SVector};
+
+/// Gaussian
+///
+/// This struct contains a mean vector and covariance matrix which represent
+/// a Gaussian distribution. The size and underlying types are statically defined
+/// at compile time.
+///
+/// # Fields
+///
+/// * `T` - Type of data held in the Gaussian
+/// * `DIM` - Dimension of the state
+/// * `mean` - The Gaussian mean vector (DIM x 1)
+/// * `cov` - The Gaussian covariance matrix (DIM x DIM)
+pub struct Gaussian<T: Scalar, const DIM: usize> {
+    pub mean: SVector<T, DIM>,
+    pub cov: SMatrix<T, DIM, DIM>,
+}
+
+impl<T, const DIM: usize> Gaussian<T, DIM> {
+    /// Initialization with default mean and covariance
+    ///
+    /// # Arguments
+    ///
+    /// # Returns
+    ///
+    /// Returns the initialized Gaussian with an all-zeros mean and identity covariance.
+    pub fn new() -> Self {
+        Self {
+            mean: SVector::zeros(),
+            cov: SMatrix::identity(),
+        }
+    }
+}
